9aa00df7eb2007b52ceab7d6efc12cf3faf25834
adithya-ss <adithya.satyanarayana@yahoo.com>
2025-06-06 11:38:29 +0530

    Add filters route and custom Jinja2 filter for rendering templates

--- api-dev-python-flask/render_templates_app.py
+++ api-dev-python-flask/render_templates_app.py
@@ -7,6 +7,16 @@ def index():
     list_of_items = ['Item 1', 'Item 2', 'Item 3']
     return render_template('index.html', list_of_items=list_of_items)
 
+@rendering_template_app.route('/filters')
+def filters():
+    sample_text = "This is a sample text to demonstrate filters in Jinja2!"
+    return render_template('filters.html', sample_text=sample_text)
+
+@rendering_template_app.template_filter('add_space_between_letters')
+def add_space_between_letters(data, my_data=None):
+    """Custom filter to add space between letters."""
+    return ' '.join(data) + my_data
+
 @rendering_template_app.route('/about')
 def about():
     return render_template('about.html')
