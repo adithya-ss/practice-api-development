a7475553a665fd51c890e7c55dea9a54951341eb
adithya-ss <adithya.satyanarayana@yahoo.com>
2025-06-06 10:54:52 +0530

    Refactor Flask app variable name from 'app' to 'learner_app' for clarity

--- api-dev-python-flask/learner_app.py
+++ api-dev-python-flask/learner_app.py
@@ -1,20 +1,20 @@
 from flask import Flask, request, make_response
 
-app = Flask(__name__)
+learner_app = Flask(__name__)
 
-@app.route('/')
+@learner_app.route('/')
 def index():
     return "Hello, World!"
 
-@app.route('/welcome')
+@learner_app.route('/welcome')
 def welcome():
     return "Welcome to the Flask API!"
 
-@app.route('/goodbye')
+@learner_app.route('/goodbye')
 def goodbye():
     return "Goodbye from the Flask API!"
 
-@app.route('/greet/<name>')
+@learner_app.route('/greet/<name>')
 def greet(name):
     my_custom_reponse = make_response(f"Hello, {name}!")
     my_custom_reponse.headers['Content-Type'] = 'text/plain'
@@ -22,7 +22,7 @@ def greet(name):
     my_custom_reponse.status_code = 202
     return my_custom_reponse
 
-@app.route('/handle_url_paramters')
+@learner_app.route('/handle_url_paramters')
 def handle_url_parameters():
     if 'name' in request.args.keys() and 'message' in request.args.keys():
         name = request.args.get('name', 'Guest')
@@ -33,5 +33,5 @@ def handle_url_parameters():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5556, debug=True)
+    learner_app.run(host='0.0.0.0', port=5556, debug=True)
 # To run the Flask application, use the command: python learner_app.py
