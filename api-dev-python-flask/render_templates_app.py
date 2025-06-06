648bc41dcb99b7173fdc4cf58fee6be67af479aa
adithya-ss <adithya.satyanarayana@yahoo.com>
2025-06-06 11:22:42 +0530

    Add about page and base template for Flask application

--- api-dev-python-flask/render_templates_app.py
+++ api-dev-python-flask/render_templates_app.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template
 rendering_template_app = Flask(__name__, template_folder='templates')
 
 @rendering_template_app.route('/')
-def hello_world():
+def index():
     list_of_items = ['Item 1', 'Item 2', 'Item 3']
     return render_template('index.html', list_of_items=list_of_items)
+
+@rendering_template_app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     rendering_template_app.run(host='0.0.0.0', debug=True)
